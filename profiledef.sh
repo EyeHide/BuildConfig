ce2138daa9a45915fc1023ad434c1b20a1c14c65
SysWhiteDev <syswhite.commercial@gmail.com>
2023-10-25 21:28:41 +0200

    Fixed install scripts

--- profiledef.sh
+++ profiledef.sh
@@ -23,5 +23,6 @@ file_permissions=(
   ["/usr/local/bin/choose-mirror"]="0:0:755"
   ["/usr/local/bin/Installation_guide"]="0:0:755"
   ["/usr/local/bin/livecd-sound"]="0:0:755"
-  ["/usr/local/bin/hideos-before"]="0:0:755"
+  ["/usr/local/bin/hideos-movefirmware"]="0:0:755"
+  ["/usr/local/bin/hideos-gnome"]="0:0:755"
 )
